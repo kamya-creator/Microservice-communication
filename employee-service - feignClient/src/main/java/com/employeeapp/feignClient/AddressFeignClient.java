e10fd73a54bfac8e444dbc6b5075a9e3876c2293
kamya-creator <kamyap2000@gmail.com>
2025-02-02 00:25:12 +0530

    Feign Client and Discovery Server

--- employee-service - feignClient/src/main/java/com/employeeapp/feignClient/AddressFeignClient.java
+++ employee-service - feignClient/src/main/java/com/employeeapp/feignClient/AddressFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "address-service", url = "http://localhost:8081/address-service")
+@FeignClient(name = "addressapp", url = "http://localhost:8081/address-service")
 public interface AddressFeignClient {
 
     @GetMapping("/address/{employeeId}")
