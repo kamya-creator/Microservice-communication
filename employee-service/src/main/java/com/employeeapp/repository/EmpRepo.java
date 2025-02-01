dc419ede592e09e307efa4349db0973012172d45
kamya-creator <kamyap2000@gmail.com>
2025-02-01 23:51:02 +0530

    discovery service

--- employee-service/src/main/java/com/employeeapp/repository/EmpRepo.java
+++
@@ -1,7 +0,0 @@
-package com.employeeapp.repository;
-
-import com.employeeapp.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpRepo extends CrudRepository<Employee, Integer> {
-}
